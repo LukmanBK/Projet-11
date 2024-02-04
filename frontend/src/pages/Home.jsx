68b7cdfa9d682b481f1c30051e54099c653465fe
LukmanBK <lukmanbakari@gmail.com>
2024-02-04 16:09:27 +0100

    Page Home terminée

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,18 +2,41 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Banner from "../components/Banner/Banner";
 import Footer from "../components/Footer/Footer";
+import Feature from "../components/Feature/Feature";
+import chatIcon from "../assets/icon-chat.png";
+import moneyIcon from "../assets/icon-money.png";
+import securityIcon from "../assets/icon-security.png";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-<main>
+      <main>
         <Banner />
- <section className="features">
+        <section className="features">
           <h2 className="sr-only">Features</h2>
+          <Feature
+            title="You are our #1 priority"
+            content="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+            imageSrc={chatIcon}
+            altText="Chat icon"
+          />
+
+          <Feature
+            title="More savings means higher rates"
+            content="The more you save with us, the higher your interest rate will be!"
+            imageSrc={moneyIcon}
+            altText="Money icon"
+          />
+
+          <Feature
+            title="Security you can trust"
+            content="We use top of the line encryption to make sure your data and money is always safe."
+            imageSrc={securityIcon}
+            altText="Security icon"
+          />
         </section>
       </main>
-
       <Footer />
     </div>
   );
